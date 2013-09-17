d2dd35b6d4d21aec0d9e7c0c6a7fc21e3198c41b
1000copy <1000copy@gmail.com>
2013-09-17 16:45:15 +0800

    -

--- share/logger.c
+++ share/logger.c
@@ -7,7 +7,7 @@
 FILE *fp ;
 static int SESSION_TRACKER; //Keeps track of session
  
-char* print_time()
+char* print_time2()
 {
     time_t t;
     char *buf;
@@ -19,6 +19,16 @@ char* print_time()
     
     return buf;
 }
+char* print_time(){
+    time_t timer;
+    char *buffer = new char[25];
+    struct tm* tm_info;
+    time(&timer);
+    tm_info = localtime(&timer);
+    strftime(buffer, 25, "%Y:%m:%d%H:%M:%S", tm_info);
+    return buffer ;
+}
+
 void log_print(char* filename, int line, char *fmt,...)
 {
     va_list         list;
@@ -29,8 +39,9 @@ void log_print(char* filename, int line, char *fmt,...)
       fp = fopen ("log.txt","a+");
     else
       fp = fopen ("log.txt","w");
-     
-    fprintf(fp,"%s ",print_time());
+    char *t = print_time();
+    fprintf(fp,"%s ",t);
+    free(t);
     va_start( list, fmt );
  
     for ( p = fmt ; *p ; ++p )
