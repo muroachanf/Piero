0af0fa11cad6fc25e69a9f4a3c6c41190e86acf6
1000copy <1000copy@gmail.com>
2013-10-18 17:27:17 +0800

    setwindowlong,getwindowlong ,第一回合

--- a-HELLOC/HELLO.C
+++ a-HELLOC/HELLO.C
@@ -7,85 +7,13 @@
 /******************************************************/
 
 #include <windows.h>
+#include "u_win.c"
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
-class app{
-	public :
-	int PASCAL WinMain(HINSTANCE hinst, LPSTR cmdline, int show)
-	{
-		HWND hwnd;
-		MSG msg;
-
-		{
-			WNDCLASS wndcls;
-
-			wndcls.style = CS_HREDRAW | CS_VREDRAW;
-			wndcls.lpfnWndProc = WndProc;
-			wndcls.cbClsExtra = 0;
-			wndcls.cbWndExtra = 0;
-			wndcls.hInstance = hinst;
-			wndcls.hIcon = LoadIcon(hinst, "appicon");
-			wndcls.hCursor = LoadCursor(NULL, IDC_ARROW);
-			wndcls.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-			wndcls.lpszMenuName = "HELLOMENU";
-			wndcls.lpszClassName = "HELLOWIN";
-
-			RegisterClass(&wndcls);
-		}
-
-		hwnd = CreateWindow("HELLOWIN",		 /* class name */
-			"HELLO--The C version",				 /* title */
-			WS_OVERLAPPEDWINDOW,					 /* window style */
-			CW_USEDEFAULT,							 /* x position */
-			CW_USEDEFAULT,							 /* y position */
-			CW_USEDEFAULT,							 /* width */
-			CW_USEDEFAULT,							 /* height */
-			NULL,										 /* parent */
-			NULL,										 /* menu */
-			hinst,									 /* module instance */
-			NULL);									 /* create param */
-
-		ShowWindow(hwnd, show);
-		UpdateWindow(hwnd);
-
-		while (GetMessage(&msg, NULL, 0, 0)) {
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-		return msg.wParam;
-	}
-};
-int PASCAL WinMain(HINSTANCE hinst, HINSTANCE , LPSTR cmdline, int show)
+int PASCAL WinMain(HINSTANCE hinst, HINSTANCE pinst, LPSTR cmdline, int show)
 {
-	app a ;
-	return a.WinMain(hinst,cmdline,show);
-}
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
-{
-	PAINTSTRUCT ps;
-	HDC hdc;
-	RECT rect;
-
-	switch(msg) {
-	case WM_PAINT:
-		hdc = BeginPaint(hwnd, &ps);
-		GetClientRect(hwnd, &rect);
-		DrawText(hdc, "Hello, world.", -1, &rect,
-			DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-		EndPaint(hwnd, &ps);
-		return 0;
-
-	case WM_COMMAND:
-		switch (wp) {
-		case IDCANCEL:
-			SendMessage(hwnd, WM_CLOSE, 0, 0);
-			break;
-		}
-		return 0;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	}
-	return DefWindowProc(hwnd, msg, wp, lp);
+	win *w = new win(hinst, pinst,show);
+	// w->set_rect(CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT);
+	w->set_rect(100,100,300,200);
+	app a (w);
+	return a.main(hinst,cmdline,show);
 }
---
+++ a-HELLOC/u_win.c
@@ -0,0 +1,113 @@
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+class win{
+	WNDPROC winproc;
+	HINSTANCE hinst;
+	HINSTANCE pinst;
+	int show;
+	int x,y,w,h;
+  public:
+  	win(HINSTANCE hinsta, HINSTANCE pinsta,int showa){
+  		winproc = WndProc ;
+  		hinst = hinsta ;
+  		show = showa;
+  		pinst = pinsta;
+  	}
+  	void set_rect(int x_,int y_,int w_,int h_){
+  		x = x_;
+  		y = y_;
+  		w = w_;
+  		h = h_;
+  	}
+  	LRESULT CALLBACK _proc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp){
+  		// ::MessageBox(NULL,"this","",0);
+  		PAINTSTRUCT ps;
+		HDC hdc;
+		RECT rect;
+
+		switch(msg) {
+		case WM_PAINT:
+			hdc = BeginPaint(hwnd, &ps);
+			GetClientRect(hwnd, &rect);
+			DrawText(hdc, "Hello, world.", -1, &rect,
+				DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+			EndPaint(hwnd, &ps);
+			return 0;
+
+		case WM_COMMAND:
+			switch (wp) {
+			case IDCANCEL:
+				SendMessage(hwnd, WM_CLOSE, 0, 0);
+				break;
+			}
+			return 0;
+
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+		}
+		return DefWindowProc(hwnd, msg, wp, lp);
+  	}
+  	void main(){
+  		HWND hwnd;
+  		if (pinst==NULL) 
+		{
+			WNDCLASS wndcls;
+
+			wndcls.style = CS_HREDRAW | CS_VREDRAW;
+			wndcls.lpfnWndProc = winproc;
+			wndcls.cbClsExtra = 0;
+			wndcls.cbWndExtra = 0;
+			wndcls.hInstance = hinst;
+			wndcls.hIcon = LoadIcon(hinst, "appicon");
+			wndcls.hCursor = LoadCursor(NULL, IDC_ARROW);
+			wndcls.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+			wndcls.lpszMenuName = "HELLOMENU";
+			wndcls.lpszClassName = "HELLOWIN";
+
+			RegisterClass(&wndcls);
+		}
+
+		hwnd = CreateWindow("HELLOWIN",		 /* class name */
+			"HELLO--The C version",				 /* title */
+			WS_OVERLAPPEDWINDOW,					 /* window style */
+			x,y,w,h,
+			// 0,0,100,100
+			NULL,										 /* parent */
+			NULL,										 /* menu */
+			hinst,									 /* module instance */
+			NULL);									 /* create param */
+		// bind win -- hwnd 
+		SetWindowLong(hwnd,GWL_USERDATA,(LONG)this);
+		ShowWindow(hwnd, show);
+		UpdateWindow(hwnd);
+  	}
+};
+class app{
+	int loop(){
+		MSG msg;
+		while (GetMessage(&msg, NULL, 0, 0)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		return msg.wParam;
+	}
+	win * w;
+	public :
+	app(win *a){
+		w = a;
+	}
+	~app(){
+		if (NULL!=w)
+			delete w;
+	}
+	int PASCAL main(HINSTANCE hinst, LPSTR cmdline, int show)
+	{		
+		w->main();
+		return loop();
+	}
+};
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	return ((win*)GetWindowLong(hwnd,GWL_USERDATA))->_proc(hwnd,msg,wp,lp);	
+}
